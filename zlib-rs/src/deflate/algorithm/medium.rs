06f354c56cde2941197dab35b7e7f83d39d5823d
Folkert <folkert@folkertdev.nl>
2024-02-09 14:27:31 +0100

    make sym_buf a ReadBuf

--- zlib-rs/src/deflate/algorithm/medium.rs
+++ zlib-rs/src/deflate/algorithm/medium.rs
@@ -163,7 +163,7 @@ pub fn deflate_medium(stream: &mut DeflateStream, flush: Flush) -> BlockState {
         return BlockState::FinishDone;
     }
 
-    if stream.state.sym_next != 0 {
+    if !stream.state.sym_buf.is_empty() {
         flush_block!(stream, false);
     }
 
