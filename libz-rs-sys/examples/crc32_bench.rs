027eebfe4fa73ac91686bd28928f6b1df72404c8
Folkert <folkert@folkertdev.nl>
2024-02-20 14:41:20 +0100

    try to benchmark an aligned adler32 variant
    it just has the same performance/instructions/branches though

--- libz-rs-sys/examples/crc32_bench.rs
+++ libz-rs-sys/examples/crc32_bench.rs
@@ -48,6 +48,22 @@ pub fn main() {
             println!("{:#x}", h.finalize());
         }
 
+        "adler32" => {
+            let path = it.next().unwrap();
+            let input = std::fs::read(path).unwrap();
+
+            let h = zlib_rs::adler32::adler32(42, &input);
+            println!("{:#x}", h);
+        }
+
+        "adler32_aligned" => {
+            let path = it.next().unwrap();
+            let input = std::fs::read(path).unwrap();
+
+            let h = zlib_rs::adler32::adler32_aligned(42, &input);
+            println!("{:#x}", h);
+        }
+
         other => panic!("invalid option '{other}', expected one of 'rs' or 'ng'"),
     }
 }
