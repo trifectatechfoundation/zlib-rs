60eb1235e994aed951ef00f7da12cdd9734fd8bb
Folkert <folkert@folkertdev.nl>
2024-04-17 17:40:24 +0200

    adler change return value for empty input

--- zlib-rs/src/adler32/neon.rs
+++ zlib-rs/src/adler32/neon.rs
@@ -30,7 +30,7 @@ pub fn adler32_neon(mut adler: u32, buf: &[u8]) -> u32 {
 
     /* initial Adler-32 value (deferred check for len == 1 speed) */
     if buf.is_empty() {
-        return 1;
+        return adler | (sum2 << 16);
     }
 
     /* in case short lengths are provided, keep it somewhat fast */
