4f5cadb8e03307f528194dbc194d883f53ef7b79
Folkert <folkert@folkertdev.nl>
2024-06-27 13:09:34 +0200

    clippy

--- dynamic-libz-sys/src/generated.rs
+++ dynamic-libz-sys/src/generated.rs
@@ -1,5 +1,6 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(clippy::too_many_arguments)]
 use std::os::raw::{c_char, c_int, c_long, c_uchar, c_uint, c_ulong, c_void};
 #[cfg(not(zng))]
 macro_rules! if_zng {
