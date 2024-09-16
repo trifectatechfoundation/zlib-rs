8ea874e433fc72857fc126e36db8fc099751cccf
Folkert de Vries <folkert@folkertdev.nl>
2024-09-16 18:48:08 +0200

    add chunked and adler32 measurements

--- test-libz-rs-sys/examples/blogpost-uncompress.rs
+++ test-libz-rs-sys/examples/blogpost-uncompress.rs
@@ -3,6 +3,59 @@
 // we use the libz_sys but configure zlib-ng in zlib compat mode
 use libz_sys as libz_ng_sys;
 
+macro_rules! chunked_body {
+    ($chunk_size:expr, $dest:expr, $source:expr) => {{
+        use core::ffi::*;
+        use zlib_rs::{inflate::InflateConfig, InflateFlush, ReturnCode};
+
+        let mut stream = core::mem::MaybeUninit::zeroed();
+
+        let err = inflateInit2_(
+            stream.as_mut_ptr(),
+            InflateConfig::default().window_bits,
+            zlibVersion(),
+            core::mem::size_of::<z_stream>() as c_int,
+        );
+        assert_eq!(err, 0);
+
+        let stream = stream.assume_init_mut();
+
+        stream.avail_out = $dest.len() as _;
+        stream.next_out = $dest.as_mut_ptr();
+
+        for chunk in $source.chunks($chunk_size) {
+            stream.next_in = chunk.as_ptr() as *mut u8;
+            stream.avail_in = chunk.len() as _;
+
+            let err = inflate(stream, InflateFlush::NoFlush as _);
+
+            if err == ReturnCode::StreamEnd as i32 {
+                break;
+            }
+
+            assert_eq!(err, ReturnCode::Ok as i32);
+        }
+
+        let err = inflateEnd(stream);
+        assert_eq!(err, ReturnCode::Ok as i32);
+
+        stream.total_out
+    }};
+}
+
+macro_rules! chunked {
+    ("chunked-ng", $chunk_size:expr, $dest:expr, $source:expr) => {{
+        use libz_sys::*;
+
+        unsafe { chunked_body!($chunk_size, $dest, $source) }
+    }};
+    ("chunked-rs", $chunk_size:expr, $dest:expr, $source:expr) => {{
+        use libz_rs_sys::*;
+
+        unsafe { chunked_body!($chunk_size, $dest, $source) }
+    }};
+}
+
 fn main() {
     let mut it = std::env::args();
 
@@ -33,6 +86,46 @@ fn main() {
 
             let err = unsafe { ::libz_rs_sys::uncompress(dest, &mut dest_len, source, source_len) };
             assert_eq!(err, 0);
+        }
+        "ng-chunked" => {
+            let chunk_log: usize = it.next().unwrap().parse().unwrap();
+
+            let path = it.next().unwrap();
+            let input = std::fs::read(&path).unwrap();
+
+            let _ = chunked!("chunked-ng", 1 << chunk_log, &mut dest_vec, &input);
+        }
+        "rs-chunked" => {
+            let chunk_log: usize = it.next().unwrap().parse().unwrap();
+
+            let path = it.next().unwrap();
+            let input = std::fs::read(&path).unwrap();
+
+            let _ = chunked!("chunked-rs", 1 << chunk_log, &mut dest_vec, &input);
+        }
+        "ng-adler" => {
+            let chunk_log: usize = it.next().unwrap().parse().unwrap();
+
+            let path = it.next().unwrap();
+            let input = std::fs::read(&path).unwrap();
+
+            let mut adler = unsafe { libz_sys::adler32(0, core::ptr::null(), 0) };
+
+            for chunk in input.chunks(1 << chunk_log) {
+                adler = unsafe { libz_sys::adler32(adler, chunk.as_ptr(), chunk.len() as _) };
+            }
+        }
+        "rs-adler" => {
+            let chunk_log: usize = it.next().unwrap().parse().unwrap();
+
+            let path = it.next().unwrap();
+            let input = std::fs::read(&path).unwrap();
+
+            let mut adler = unsafe { libz_rs_sys::adler32(0, core::ptr::null(), 0) };
+
+            for chunk in input.chunks(1 << chunk_log) {
+                adler = unsafe { libz_rs_sys::adler32(adler, chunk.as_ptr(), chunk.len() as _) };
+            }
         }
         other => panic!("invalid option '{other}', expected one of 'rs' or 'ng'"),
     }
