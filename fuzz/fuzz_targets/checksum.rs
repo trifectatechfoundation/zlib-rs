0ae564c601ed1b514830bc2d6c30d2696f1b6607
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-10-14 09:58:36 +0200

    Fix checksum fuzzing target

--- fuzz/fuzz_targets/checksum.rs
+++ fuzz/fuzz_targets/checksum.rs
@@ -21,23 +21,6 @@ fuzz_target!(|input: (Vec<u8>, u32)| {
     }
 
     {
-        let expected = {
-            let mut h = crc32fast::Hasher::new_with_initial(0);
-            h.update(&input[..]);
-            h.finalize()
-        };
-
-        let mut buf = [0; 1 << 16];
-        let mut dst = zlib_rs::read_buf::ReadBuf::new(&mut buf[..input.len()]);
-
-        let actual = zlib_rs::crc32::crc32_copy(&mut dst, input.as_slice());
-
-        assert_eq!(expected, actual);
-
-        assert_eq!(input, dst.filled());
-    }
-
-    {
         use zlib_rs::{crc32, crc32_combine};
 
         let data = &input;
