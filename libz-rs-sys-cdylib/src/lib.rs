4e5937d8c856c7b340688ae70ae663bfce0e9530
Brian Pane <brianp@brianp.net>
2025-05-19 17:02:07 -0700

    Move gz API to libz-rs-sys-cdylib

--- libz-rs-sys-cdylib/src/lib.rs
+++ libz-rs-sys-cdylib/src/lib.rs
@@ -4,3 +4,9 @@ pub use libz_rs_sys::*;
 
 #[cfg(not(panic = "abort"))]
 compile_error!("panic=\"abort\" is mandatory because unwinding to C is undefined behavior");
+
+#[cfg(feature = "gz")]
+mod gz;
+
+#[cfg(feature = "gz")]
+pub use gz::*;
