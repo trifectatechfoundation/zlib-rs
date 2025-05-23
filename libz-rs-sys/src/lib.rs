1edac3922cdcd7ef7716434ea36bff44af831343
Folkert de Vries <folkert@folkertdev.nl>
2025-05-23 12:10:39 +0200

    keep `inflateInit2` private for now

--- libz-rs-sys/src/lib.rs
+++ libz-rs-sys/src/lib.rs
@@ -663,7 +663,7 @@ pub unsafe extern "C-unwind" fn inflateInit2_(
 ///     - `zalloc`
 ///     - `zfree`
 ///     - `opaque`
-pub unsafe extern "C-unwind" fn inflateInit2(strm: z_streamp, windowBits: c_int) -> c_int {
+unsafe extern "C-unwind" fn inflateInit2(strm: z_streamp, windowBits: c_int) -> c_int {
     let Some(strm) = (unsafe { strm.as_mut() }) else {
         return ReturnCode::StreamError as _;
     };
