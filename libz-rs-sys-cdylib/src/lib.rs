eb41cd2133a9ac51152605e43eee67e2020d31a1
Folkert de Vries <folkert@folkertdev.nl>
2025-05-27 14:01:45 +0200

    add `__internal-test` feature for `cdylib` to work around `panic=abort`

--- libz-rs-sys-cdylib/src/lib.rs
+++ libz-rs-sys-cdylib/src/lib.rs
@@ -2,9 +2,8 @@ extern crate libz_rs_sys;
 
 pub use libz_rs_sys::*;
 
-// TODO somehow make this work with the tests
-// #[cfg(not(panic = "abort"))]
-// compile_error!("panic=\"abort\" is mandatory because unwinding to C is undefined behavior");
+#[cfg(not(any(panic = "abort", feature = "__internal-test", test, doc, miri)))]
+compile_error!("panic=\"abort\" is mandatory because unwinding to C is undefined behavior");
 
 #[cfg(feature = "gz")]
 mod gz;
