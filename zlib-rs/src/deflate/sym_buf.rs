d2c4ba1451ee2ead76df2704215138736aec634e
Folkert de Vries <folkert@folkertdev.nl>
2025-11-28 15:35:51 +0100

    use only a single allocation for all deflate state

--- zlib-rs/src/deflate/sym_buf.rs
+++ zlib-rs/src/deflate/sym_buf.rs
@@ -1,7 +1,6 @@
 // taken from https://docs.rs/tokio/latest/src/tokio/io/read_buf.rs.html#23-27
 // based on https://rust-lang.github.io/rfcs/2930-read-buf.html
 
-use crate::allocate::Allocator;
 use crate::weak_slice::WeakSliceMut;
 
 pub(crate) struct SymBuf<'a> {
@@ -60,31 +59,18 @@ impl<'a> SymBuf<'a> {
         self.filled += 3;
     }
 
-    pub(crate) fn new_in(alloc: &Allocator<'a>, len: usize) -> Option<Self> {
-        let ptr = alloc.allocate_zeroed_buffer(len * 3)?;
-
-        // safety: all elements are now initialized
-        let buf = unsafe { WeakSliceMut::from_raw_parts_mut(ptr.as_ptr(), len * 3) };
-
-        Some(Self { buf, filled: 0 })
+    pub(crate) unsafe fn from_raw_parts(ptr: *mut u8, lit_bufsize: usize) -> Self {
+        Self {
+            buf: unsafe { WeakSliceMut::from_raw_parts_mut(ptr, lit_bufsize * 3) },
+            filled: 0,
+        }
     }
 
-    pub(crate) fn clone_in(&self, alloc: &Allocator<'a>) -> Option<Self> {
-        let mut clone = Self::new_in(alloc, self.buf.len() / 3)?;
-
-        clone
-            .buf
-            .as_mut_slice()
-            .copy_from_slice(self.buf.as_slice());
-        clone.filled = self.filled;
-
-        Some(clone)
-    }
-
-    pub(crate) unsafe fn drop_in(&mut self, alloc: &Allocator<'a>) {
-        if !self.buf.is_empty() {
-            let mut buf = core::mem::replace(&mut self.buf, WeakSliceMut::empty());
-            unsafe { alloc.deallocate(buf.as_mut_ptr(), buf.len()) }
+    pub(crate) unsafe fn clone_to(&self, ptr: *mut u8) -> Self {
+        unsafe { ptr.copy_from_nonoverlapping(self.buf.as_ptr(), self.buf.len()) };
+        Self {
+            buf: unsafe { WeakSliceMut::from_raw_parts_mut(ptr, self.buf.len()) },
+            filled: self.filled,
         }
     }
 }
