f13ebe49c71caa755b0796b9c71a788eb9b9c328
Folkert de Vries <folkert@folkertdev.nl>
2026-03-31 19:21:03 +0200

    fix aliasing bug in `infback`

--- zlib-rs/src/inflate/window.rs
+++ zlib-rs/src/inflate/window.rs
@@ -83,6 +83,10 @@ impl<'a> Window<'a> {
         self.buf.as_ptr()
     }
 
+    pub fn as_mut_ptr(&mut self) -> *mut u8 {
+        self.buf.as_mut_ptr()
+    }
+
     #[cfg(test)]
     fn extend_adler32(&mut self, slice: &[u8], checksum: &mut u32) {
         self.extend(slice, 0, true, checksum, &mut Crc32Fold::new());
